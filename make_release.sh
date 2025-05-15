64198ad1e43c441bd28a62dc51429e66dae536e1
tgraphite@laptop <root@Graphite>
2025-05-15 10:25:46 +0800

    1.8

--- make_release.sh
+++ make_release.sh
@@ -11,6 +11,9 @@ mkdir reax_tools_linux
 cp build/reax_tools reax_tools_linux/
 cp -r lib reax_tools_linux/
 cp doc/tutorial_1_energetic_materials.md reax_tools_linux/
-cp doc/tutorial_2_energetic_materials.pdf reax_tools_linux/
+cp doc/tutorial_1_energetic_materials.pdf reax_tools_linux/
+cp plot/* reax_tools_linux/
 
-tar -cjf reax_tools_linux.tar.gz reax_tools_linux
+tar -cjvf reax_tools_linux.tar.gz reax_tools_linux
+
+rm -rf reax_tools_linux
