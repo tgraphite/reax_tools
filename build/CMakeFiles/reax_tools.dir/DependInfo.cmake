3b64019ae300ff152442b2a704e750d66e54c7e7
tgraphite@laptop <root@Graphite.localdomain>
2026-03-27 06:40:49 +0800

    release 2.0

---
+++ build/CMakeFiles/reax_tools.dir/DependInfo.cmake
@@ -0,0 +1,30 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/mnt/d/CurrentProjects/reax_tools/cpp/argparser.cpp" "CMakeFiles/reax_tools.dir/cpp/argparser.cpp.o" "gcc" "CMakeFiles/reax_tools.dir/cpp/argparser.cpp.o.d"
+  "/mnt/d/CurrentProjects/reax_tools/cpp/cell_list.cpp" "CMakeFiles/reax_tools.dir/cpp/cell_list.cpp.o" "gcc" "CMakeFiles/reax_tools.dir/cpp/cell_list.cpp.o.d"
+  "/mnt/d/CurrentProjects/reax_tools/cpp/fmt/format.cc" "CMakeFiles/reax_tools.dir/cpp/fmt/format.cc.o" "gcc" "CMakeFiles/reax_tools.dir/cpp/fmt/format.cc.o.d"
+  "/mnt/d/CurrentProjects/reax_tools/cpp/fmt/os.cc" "CMakeFiles/reax_tools.dir/cpp/fmt/os.cc.o" "gcc" "CMakeFiles/reax_tools.dir/cpp/fmt/os.cc.o.d"
+  "/mnt/d/CurrentProjects/reax_tools/cpp/main.cpp" "CMakeFiles/reax_tools.dir/cpp/main.cpp.o" "gcc" "CMakeFiles/reax_tools.dir/cpp/main.cpp.o.d"
+  "/mnt/d/CurrentProjects/reax_tools/cpp/rdkit_utils.cpp" "CMakeFiles/reax_tools.dir/cpp/rdkit_utils.cpp.o" "gcc" "CMakeFiles/reax_tools.dir/cpp/rdkit_utils.cpp.o.d"
+  "/mnt/d/CurrentProjects/reax_tools/cpp/reax_counter.cpp" "CMakeFiles/reax_tools.dir/cpp/reax_counter.cpp.o" "gcc" "CMakeFiles/reax_tools.dir/cpp/reax_counter.cpp.o.d"
+  "/mnt/d/CurrentProjects/reax_tools/cpp/reax_flow.cpp" "CMakeFiles/reax_tools.dir/cpp/reax_flow.cpp.o" "gcc" "CMakeFiles/reax_tools.dir/cpp/reax_flow.cpp.o.d"
+  "/mnt/d/CurrentProjects/reax_tools/cpp/reax_track.cpp" "CMakeFiles/reax_tools.dir/cpp/reax_track.cpp.o" "gcc" "CMakeFiles/reax_tools.dir/cpp/reax_track.cpp.o.d"
+  "/mnt/d/CurrentProjects/reax_tools/cpp/string_tools.cpp" "CMakeFiles/reax_tools.dir/cpp/string_tools.cpp.o" "gcc" "CMakeFiles/reax_tools.dir/cpp/string_tools.cpp.o.d"
+  "/mnt/d/CurrentProjects/reax_tools/cpp/system.cpp" "CMakeFiles/reax_tools.dir/cpp/system.cpp.o" "gcc" "CMakeFiles/reax_tools.dir/cpp/system.cpp.o.d"
+  "/mnt/d/CurrentProjects/reax_tools/cpp/universe.cpp" "CMakeFiles/reax_tools.dir/cpp/universe.cpp.o" "gcc" "CMakeFiles/reax_tools.dir/cpp/universe.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/reax_tools.dir/cmake_clean.cmake
@@ -0,0 +1,33 @@
+file(REMOVE_RECURSE
+  "/mnt/d/CurrentProjects/reax_tools/bin/reax_tools"
+  "/mnt/d/CurrentProjects/reax_tools/bin/reax_tools.pdb"
+  "CMakeFiles/reax_tools.dir/cpp/argparser.cpp.o"
+  "CMakeFiles/reax_tools.dir/cpp/argparser.cpp.o.d"
+  "CMakeFiles/reax_tools.dir/cpp/cell_list.cpp.o"
+  "CMakeFiles/reax_tools.dir/cpp/cell_list.cpp.o.d"
+  "CMakeFiles/reax_tools.dir/cpp/fmt/format.cc.o"
+  "CMakeFiles/reax_tools.dir/cpp/fmt/format.cc.o.d"
+  "CMakeFiles/reax_tools.dir/cpp/fmt/os.cc.o"
+  "CMakeFiles/reax_tools.dir/cpp/fmt/os.cc.o.d"
+  "CMakeFiles/reax_tools.dir/cpp/main.cpp.o"
+  "CMakeFiles/reax_tools.dir/cpp/main.cpp.o.d"
+  "CMakeFiles/reax_tools.dir/cpp/rdkit_utils.cpp.o"
+  "CMakeFiles/reax_tools.dir/cpp/rdkit_utils.cpp.o.d"
+  "CMakeFiles/reax_tools.dir/cpp/reax_counter.cpp.o"
+  "CMakeFiles/reax_tools.dir/cpp/reax_counter.cpp.o.d"
+  "CMakeFiles/reax_tools.dir/cpp/reax_flow.cpp.o"
+  "CMakeFiles/reax_tools.dir/cpp/reax_flow.cpp.o.d"
+  "CMakeFiles/reax_tools.dir/cpp/reax_track.cpp.o"
+  "CMakeFiles/reax_tools.dir/cpp/reax_track.cpp.o.d"
+  "CMakeFiles/reax_tools.dir/cpp/string_tools.cpp.o"
+  "CMakeFiles/reax_tools.dir/cpp/string_tools.cpp.o.d"
+  "CMakeFiles/reax_tools.dir/cpp/system.cpp.o"
+  "CMakeFiles/reax_tools.dir/cpp/system.cpp.o.d"
+  "CMakeFiles/reax_tools.dir/cpp/universe.cpp.o"
+  "CMakeFiles/reax_tools.dir/cpp/universe.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/reax_tools.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
